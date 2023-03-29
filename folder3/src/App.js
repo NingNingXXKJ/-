e4ea4a5f080e5e434664cd0ec2ec41447c171ba0
KainingGao <kygao22@gmail.com>
2023-03-29 05:41:43 +0000

    Create the main navigation component

---
+++ folder3/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+
+
+import UseStateExample from './components/UseStateExample';
+import UseEffectExample from './components/UseEffectExample';
+import UseContextExample from './components/UseContextExample';
+import UseRefExample from './components/UseRefExample';
+import UseCallbackExample from './components/UseCallbackExample';
+import UseMemoExample from './components/UseMemoExample';
+import UseReducerExample from './components/UseReducerExample';
+import CustomHookExample from './components/CustomHookExample';
+
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li><Link to="/use-state">useState</Link></li>
+        <li><Link to="/use-effect">useEffect</Link></li>
+        <li><Link to="/use-context">useContext</Link></li>
+        <li><Link to="/use-ref">useRef</Link></li>
+        <li><Link to="/use-callback">useCallback</Link></li>
+        <li><Link to="/use-memo">useMemo</Link></li>
+        <li><Link to="/use-reducer">useReducer</Link></li>
+        <li><Link to="/custom-hook">Custom Hook</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
