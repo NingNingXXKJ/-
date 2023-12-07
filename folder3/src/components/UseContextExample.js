dff8c3f93a3f04f01a107db6748c30c7b94a6d6c
KainingGao <kygao22@gmail.com>
2023-12-07 18:43:44 +0000

    Toggle theme function

---
+++ folder3/src/components/UseContextExample.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from 'react';
+
+
+const ThemeContext = createContext();
+
+
+const UserContext = createContext();
+
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
