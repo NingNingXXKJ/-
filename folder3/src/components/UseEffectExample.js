8dc4e1d865ee4c47e55f923c70efb18242978717
KainingGao <kygao22@gmail.com>
2024-04-22 06:46:58 +0000

    Initialize state variables for different examples

---
+++ folder3/src/components/UseEffectExample.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+
+const UseEffectExample = () => {
+
+  const [count, setCount] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
