6fcae42cd8fa56a31f04a4154a50d0c54fee2ce5
KainingGao <kygao22@gmail.com>
2024-07-25 23:33:22 +0000

    Define action types for the reducer

---
+++ folder3/src/components/UseReducerExample.js
@@ -0,0 +1,10 @@
+import React, { useReducer, useState } from 'react';
+
+
+const ACTIONS = {
+  ADD_TODO: 'ADD_TODO',
+  TOGGLE_TODO: 'TOGGLE_TODO',
+  DELETE_TODO: 'DELETE_TODO',
+  SET_FILTER: 'SET_FILTER'
+};
+
