b4d9761cb2c3eab7f7b30c78749b806c0850df52
KainingGao <kygao22@gmail.com>
2025-02-01 12:42:32 +0000

    Import all hook example components

--- folder1/src/App.js
+++ folder1/src/App.js
@@ -1,4 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
+import UseStateExample from './components/UseStateExample';
+import UseEffectExample from './components/UseEffectExample';
+import UseContextExample from './components/UseContextExample';
+import UseRefExample from './components/UseRefExample';
+import UseCallbackExample from './components/UseCallbackExample';
+import UseMemoExample from './components/UseMemoExample';
+import UseReducerExample from './components/UseReducerExample';
+import CustomHookExample from './components/CustomHookExample';
 
