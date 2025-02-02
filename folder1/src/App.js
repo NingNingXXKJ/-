a1fb6d548fc6b9e04ed097fa0151def8a49c530b
KainingGao <kygao22@gmail.com>
2025-02-02 04:52:48 +0000

    Define the main App component with routing setup

--- folder1/src/App.js
+++ folder1/src/App.js
@@ -1,17 +1,27 @@
-function Navigation() {
+function App() {
   return (
-    <nav className="navigation">
-      <ul>
-        <li><Link to="/use-state">useState</Link></li>
-        <li><Link to="/use-effect">useEffect</Link></li>
-        <li><Link to="/use-context">useContext</Link></li>
-        <li><Link to="/use-ref">useRef</Link></li>
-        <li><Link to="/use-callback">useCallback</Link></li>
-        <li><Link to="/use-memo">useMemo</Link></li>
-        <li><Link to="/use-reducer">useReducer</Link></li>
-        <li><Link to="/custom-hook">Custom Hook</Link></li>
-      </ul>
-    </nav>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Hooks Guide</h1>
+          <Navigation />
+        </header>
+        
+        <main className="content">
+          <Routes>
+            <Route path="/use-state" element={<UseStateExample />} />
+            <Route path="/use-effect" element={<UseEffectExample />} />
+            <Route path="/use-context" element={<UseContextExample />} />
+            <Route path="/use-ref" element={<UseRefExample />} />
+            <Route path="/use-callback" element={<UseCallbackExample />} />
+            <Route path="/use-memo" element={<UseMemoExample />} />
+            <Route path="/use-reducer" element={<UseReducerExample />} />
+            <Route path="/custom-hook" element={<CustomHookExample />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
-
+
+export default App;
