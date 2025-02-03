fadf2cdc6af90da8498152f94e5dd1b235737e59
KainingGao <kygao22@gmail.com>
2025-02-03 10:39:38 +0000

    Handle form input changes using object state

---
+++ folder3/src/components/UseStateExample.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+const UseStateExample = () => {
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  });
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
