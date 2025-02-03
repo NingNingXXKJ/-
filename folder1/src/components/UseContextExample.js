be0084fac91314dc4255149e2420b52a6eb8f74e
KainingGao <kygao22@gmail.com>
2025-02-03 10:27:39 +0000

    Handle toggling notifications

--- folder1/src/components/UseContextExample.js
+++ folder1/src/components/UseContextExample.js
@@ -1,3 +1,23 @@
-const UserProfile = () => {
-  const { user, setUser } = useContext(UserContext);
-  
+  const toggleNotifications = () => {
+    setUser(prevUser => ({
+      ...prevUser,
+      preferences: {
+        ...prevUser.preferences,
+        notifications: !prevUser.preferences.notifications
+      }
+    }));
+  };
+
+  return (
+    <div>
+      <h3>User Profile</h3>
+      <p>Name: {user.name}</p>
+      <p>Language: {user.preferences.language}</p>
+      <p>Notifications: {user.preferences.notifications ? 'Enabled' : 'Disabled'}</p>
+      <button className="button" onClick={toggleNotifications}>
+        Toggle Notifications
+      </button>
+    </div>
+  );
+};
+
