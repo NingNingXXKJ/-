42f186ffc8b784c59a8aa8a21f4a51ba130f6016
KainingGao <kygao22@gmail.com>
2025-02-03 06:44:10 +0000

    Create a user profile component using context

--- folder1/src/components/UseContextExample.js
+++ folder1/src/components/UseContextExample.js
@@ -1,17 +1,3 @@
-const ThemedButton = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-  
-  return (
-    <button 
-      className="button"
-      onClick={toggleTheme}
-      style={{ 
-        backgroundColor: theme === 'light' ? '#61dafb' : '#282c34',
-        color: theme === 'light' ? 'black' : 'white'
-      }}
-    >
-      Toggle Theme
-    </button>
-  );
-};
-
+const UserProfile = () => {
+  const { user, setUser } = useContext(UserContext);
+  
