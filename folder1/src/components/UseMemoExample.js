62e2a0c71912747fcee138085f6d8d03defa9fad
KainingGao <kygao22@gmail.com>
2025-02-07 21:10:31 +0000

    Memoize the expensive calculation

--- folder1/src/components/UseMemoExample.js
+++ folder1/src/components/UseMemoExample.js
@@ -1,12 +1,4 @@
-  const expensiveCalculation = (nums) => {
-    console.log('Performing expensive calculation...');
-    return nums.reduce((acc, curr) => {
-      // Simulate complex calculation
-      let result = acc;
-      for (let i = 0; i < 1000000; i++) {
-        result += curr * multiplier;
-      }
-      return result;
-    }, 0);
-  };
+  const memoizedResult = useMemo(() => {
+    return expensiveCalculation(numbers);
+  }, [numbers, multiplier]);
 
