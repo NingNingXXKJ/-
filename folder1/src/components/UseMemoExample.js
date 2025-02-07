e783fe81eb645d597ac5b9b1cac5099ba39f193b
KainingGao <kygao22@gmail.com>
2025-02-07 16:15:01 +0000

    Expensive calculation function

--- folder1/src/components/UseMemoExample.js
+++ folder1/src/components/UseMemoExample.js
@@ -1,4 +1,12 @@
-  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
-  const [multiplier, setMultiplier] = useState(1);
-  const [darkMode, setDarkMode] = useState(false);
+  const expensiveCalculation = (nums) => {
+    console.log('Performing expensive calculation...');
+    return nums.reduce((acc, curr) => {
+      // Simulate complex calculation
+      let result = acc;
+      for (let i = 0; i < 1000000; i++) {
+        result += curr * multiplier;
+      }
+      return result;
+    }, 0);
+  };
 
