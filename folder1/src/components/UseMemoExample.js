ddca4b447bd1681b146f3c03c5099b614a5535b1
KainingGao <kygao22@gmail.com>
2025-02-07 13:45:23 +0000

    Initialize state variables

--- folder1/src/components/UseMemoExample.js
+++ folder1/src/components/UseMemoExample.js
@@ -1,1 +1,4 @@
-const UseMemoExample = () => {
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const [multiplier, setMultiplier] = useState(1);
+  const [darkMode, setDarkMode] = useState(false);
+
