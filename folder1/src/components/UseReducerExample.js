7f673ce122468607d400599dd011e1c1f8fa6e8d
KainingGao <kygao22@gmail.com>
2025-02-10 14:32:31 +0000

    Define reducer function to handle state updates

--- folder1/src/components/UseReducerExample.js
+++ folder1/src/components/UseReducerExample.js
@@ -1,5 +1,39 @@
-const initialState = {
-  todos: [],
-  filter: 'ALL' // ALL, ACTIVE, COMPLETED
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, {
+          id: Date.now(),
+          text: action.payload,
+          completed: false
+        }]
+      };
+    
+    case ACTIONS.TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map(todo =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        )
+      };
+    
+    case ACTIONS.DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.payload)
+      };
+    
+    case ACTIONS.SET_FILTER:
+      return {
+        ...state,
+        filter: action.payload
+      };
+    
+    default:
+      return state;
+  }
 };
 
