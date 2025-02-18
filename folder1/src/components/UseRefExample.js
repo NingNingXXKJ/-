2fc5f3aade19340cac6b80f4b5d7450d789233ed
KainingGao <kygao22@gmail.com>
2025-02-18 03:51:20 +0000

    Create refs for DOM elements and values

--- folder1/src/components/UseRefExample.js
+++ folder1/src/components/UseRefExample.js
@@ -1,1 +1,6 @@
-const UseRefExample = () => {
+  const inputRef = useRef(null);
+  const countRef = useRef(0);
+  const intervalRef = useRef(null);
+  const [timer, setTimer] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+
