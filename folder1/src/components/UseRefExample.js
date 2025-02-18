50352583c8ae444aff40c40922aa2fed82288461
KainingGao <kygao22@gmail.com>
2025-02-18 18:54:12 +0000

    UseEffect for handling the timer

--- folder1/src/components/UseRefExample.js
+++ folder1/src/components/UseRefExample.js
@@ -1,6 +1,8 @@
-  const inputRef = useRef(null);
-  const countRef = useRef(0);
-  const intervalRef = useRef(null);
-  const [timer, setTimer] = useState(0);
-  const [inputValue, setInputValue] = useState('');
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
 
