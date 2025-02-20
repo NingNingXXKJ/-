7fd2377e84f9d79dd2010026db576e1ca3c34a20
KainingGao <kygao22@gmail.com>
2025-02-20 18:40:51 +0000

    Start timer function using ref to store interval

--- folder1/src/components/UseRefExample.js
+++ folder1/src/components/UseRefExample.js
@@ -1,4 +1,7 @@
-  const focusInput = () => {
-    inputRef.current.focus();
+  const startTimer = () => {
+    if (intervalRef.current !== null) return;
+    intervalRef.current = setInterval(() => {
+      setTimer(prev => prev + 1);
+    }, 1000);
   };
 
