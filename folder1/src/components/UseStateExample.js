6211b97baf641e75db5db60244ae1a9f822fa7d8
KainingGao <kygao22@gmail.com>
2025-02-26 14:21:56 +0000

    Handle form input changes using object state

--- folder1/src/components/UseStateExample.js
+++ folder1/src/components/UseStateExample.js
@@ -1,8 +1,8 @@
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-  const [items, setItems] = useState([]);
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
 
